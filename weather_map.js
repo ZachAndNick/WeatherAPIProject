4f1faa768cda004e97c05b101e978490a0c739a1
zacharyramos <zachary.ramos9@gmail.com>
2017-09-07 10:31:09 -0500

    created forecast object with relevant properties

--- weather_map.js
+++ weather_map.js
@@ -1,5 +1,5 @@
 (function(){
-    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+    $.get("http://api.openweathermap.org/data/2.5/forecast/", {
         APPID: "e9545dca3bd8784069868855b7f24779",
         id:     "4726206",
         units: "imperial"
@@ -7,5 +7,21 @@
     }).done(function(data){
         console.log(data);
 
+        var forecast = {
+            highTemp: data.list[0].main.temp_max,
+            lowTemp: data.list[0].main.temp_min,
+            cloudIcon: data.list[0].weather[0].icon,
+            cloudDescription: data.list[0].weather[0].description,
+            humidity: data.list[0].main.humidity,
+            wind: data.list[0].wind.speed,
+            pressure: data.list[0].main.pressure
+        }
+
+        console.log(forecast)
+
+
+
     });
+
+
 })();
